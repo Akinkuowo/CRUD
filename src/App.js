167f3ce1eae89dab80d927a2227348dbefcdccb1
Akinkuowo Oluwaseun <akinseun1990@gmail.com>
2019-12-27 11:59:35 +0100

    Updating app.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ class App extends React.Component {
 			loading: true
 		}
 
-		this.apiUrl = 'http://5e053efa2f5dff0014f7da10.mockapi.io';
+		this.apiUrl = 'https:
+//5e053efa2f5dff0014f7da10.mockapi.io';
 		
 		this.handleUpdateTodo = this.handleUpdateTodo.bind(this)	
 
